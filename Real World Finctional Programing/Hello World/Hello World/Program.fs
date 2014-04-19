e228c0acee499920f2a8cc851169a64dfc7b5dda
Sergei Golos <sergei.golos@gmail.com>
2014-04-19 12:46:43 -0400

    First Hello World
    Followed the steps in book to executing code in F#

---
+++ Real World Finctional Programing/Hello World/Hello World/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv =  
+    let great = 
+        let message = "Hello World!"
+        printfn "%s" message 
+    great
+    0 // return an integer exit code
+

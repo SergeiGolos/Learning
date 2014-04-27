ea3e2c93b4f5043a1b1140849b3addea1e91599a
Sergei Golos <sergei.golos@gmail.com>
2014-04-27 17:03:18 -0400

    chapter 4 programming example

--- Real World Finctional Programing/chapter4/chapter4/Script.fsx
+++ Real World Finctional Programing/chapter4/chapter4/Script.fsx
@@ -4,5 +4,10 @@
 #load "Library1.fs"
 open chapter4
 
+
 // Define your library scripting code here
 
+let convertDataRow(item:string) =
+    match List.ofSeq(item.Split(',')) with
+    | title::value::_ -> (title, System.Int32.Parse(value))
+    | _ -> raise( new System.Exception("Incorect format"))
